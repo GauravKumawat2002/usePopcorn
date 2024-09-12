14c6720f92161429410bbfe402e442d84ce82e72
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-09-12 12:42:22 +0530

    Remove: unnecessary code

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,6 @@ import StarRating from "./components/star-rating.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-    {/* <StarRating
-      numStars={5}
-      defaultRating={3}
-      messages={["Maa chudwa", "Bad", "Ok", "Good", "Loved It"]}
-    /> */}
   </React.StrictMode>
 );
 // function Test() {
