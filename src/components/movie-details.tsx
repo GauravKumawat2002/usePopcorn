36ed354d7dc98a9738d678edf6f96bc0bc9c9b01
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-09-12 23:10:56 +0530

    Changes: Adds multiple functionalities to the component

--- src/components/movie-details.tsx
+++ src/components/movie-details.tsx
@@ -2,18 +2,31 @@ import { useEffect, useState } from "react";
 import { KEY, SelectedMovie, WatchedMovie } from "../interfaces";
 import StarRating from "./star-rating";
 
+// MovieDetails component
 export default function MovieDetails({
   selectedMovieId,
   onCloseMovie,
-  onAddWatched, //handleAddWatched function will be passed as a prop to the MovieDetails component in this prop
+  onAddWatched, // handleAddWatched function will be passed as a prop to the MovieDetails component in this prop
+  watched,
 }: {
   selectedMovieId: string;
   onCloseMovie: () => void;
   onAddWatched: (movie: WatchedMovie) => void;
+  watched: WatchedMovie[];
 }) {
+  // State variables
   const [selectedMovie, setSelectedMovie] = useState<SelectedMovie | {}>({});
+  const [userRating, setUserRating] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
+
+  // Check if the selected movie is already in the watched list
+  const isWatched = watched.map(movie => movie.imdbID).includes(selectedMovieId);
+
+  // Get the user rating of the selected movie
+  const watchedUserRating = watched.find(movie => movie.imdbID === selectedMovieId)?.userRating;
+
+  // Destructure properties from selectedMovie
   const {
     Actors: actors,
     Poster: posters,
@@ -25,6 +38,8 @@ export default function MovieDetails({
     Genre: genre,
     Plot: plot,
   } = selectedMovie as SelectedMovie;
+
+  // Fetch movie details when selectedMovieId changes
   useEffect(() => {
     const controller = new AbortController();
     async function getMoviesDetails() {
@@ -39,6 +54,7 @@ export default function MovieDetails({
         const data = await response.json();
         if (data.Response === "False") throw new Error(data.Error);
         setSelectedMovie(data);
+        if (selectedMovieId) document.title = `🍿 ${data.Title}`;
       } catch (err) {
         // Ignore abort errors
         if (err instanceof Error && err.name !== "AbortError") {
@@ -52,23 +68,43 @@ export default function MovieDetails({
     selectedMovieId && getMoviesDetails();
     return () => {
       controller.abort();
+      document.title = "🍿 usePopcorn";
     };
   }, [selectedMovieId]);
-  console.log(selectedMovie);
 
+  // Close the movie details component when the user presses the escape key
+  useEffect(() => {
+    function closeMovieOnEsc(e: KeyboardEvent) {
+      if (e.code === "Escape") {
+        onCloseMovie();
+      }
+    }
+
+    document.addEventListener("keydown", closeMovieOnEsc);
+    return () => document.removeEventListener("keydown", closeMovieOnEsc);
+  }, [selectedMovieId]);
+
+  // Create a new watched movie object
   const newWatchedMovie: WatchedMovie = {
     imdbID: selectedMovieId,
     Poster: posters,
     Title: title,
     imdbRating: imdbRating,
     runtime: runtime,
-    userRating: "0",
+    userRating: userRating,
     Year: released,
   };
-  // thsi function will take the onAddWatched function prop and pass the newWatchedMovie to it which will add the movie to the watched list
+
+  // Function to handle adding the movie to the watched list
   function handleWatched() {
     onAddWatched(newWatchedMovie);
   }
+
+  // Function to handle setting the user rating
+  const handleSetUserRating = (rating: number) => {
+    setUserRating(rating.toString());
+  };
+
   return (
     <div className="details">
       {loading && <h2 className="loader">Loading...</h2>}
@@ -84,7 +120,6 @@ export default function MovieDetails({
               <p>
                 {released} &bull; {runtime}
               </p>
-
               <p>{genre}</p>
               <p>
                 <span>⭐</span>
@@ -93,15 +128,25 @@ export default function MovieDetails({
             </div>
           </header>
           <section>
-            <StarRating numStars={10} />
-            <button
-              className="btn-add"
-              onClick={() => {
-                handleWatched();
-                onCloseMovie();
-              }}>
-              Add to Watched
-            </button>
+            <div className="rating">
+              {!isWatched ? (
+                <>
+                  <StarRating numStars={10} setExternalRating={handleSetUserRating} />
+                  {parseFloat(userRating) > 0 && (
+                    <button
+                      className="btn-add"
+                      onClick={() => {
+                        handleWatched();
+                        onCloseMovie();
+                      }}>
+                      Add to Watched
+                    </button>
+                  )}
+                </>
+              ) : (
+                <p>You rated it {watchedUserRating} </p>
+              )}
+            </div>
             <p>
               <em>{plot}</em>
             </p>
